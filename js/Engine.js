ba2ed72e28278caa44557fe6817218727fc4eaf7
vergilius <michal.zurek@arteverest.pl>
2015-11-28 17:03:03 +0100

    Fallback set to svg directly / no canvas on mobile

--- js/Engine.js
+++ js/Engine.js
@@ -2,20 +2,45 @@
   'use strict';
 
   var CANVAS_DOM_SELECTOR = '.js-mask';
+  var BREAKPOINT = 480;
 
   var Engine = root.Engine = function Engine() {
     this.surface = null;
     this.canvas = null;
     this.context = null;
+    this.warmedUp = false;
 
-    this.warmUp();
-    this.run();
+    if (!this.maybeStart()) {
+      window.addEventListener('resize', this.maybeStart.bind(this));
+    }
+  };
+
+  Engine.prototype.maybeStart = function() {
+    if (!this.warmedUp && this.check()) {
+      this.warmUp();
+      this.run();
+
+      return true;
+    }
+    return false;
+  };
+
+  Engine.prototype.check = function() {
+    var valid = false;
+
+    if (window.innerWidth > BREAKPOINT) {
+      valid = true;
+    }
+
+    return valid;
   };
 
   Engine.prototype.warmUp = function() {
     this.surface = new Surface();
     this.canvas = document.querySelector(CANVAS_DOM_SELECTOR);
     this.context = this.canvas.getContext('2d');
+
+    this.warmedUp = true;
   };
 
   Engine.prototype.run = function() {
